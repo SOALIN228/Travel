920b5c5dbedeb1e563c1ca0ba7599f24fc0ce68e
lml <soalin228@163.com>
2019-06-27 15:18:37 +0800

    detail 页面 header 添加事件解绑

--- src/page/detail/components/Header.vue
+++ src/page/detail/components/Header.vue
@@ -53,6 +53,9 @@ export default {
   },
   activated () {
     window.addEventListener('scroll', this.handleScroll)
+  },
+  deactivated () {
+    window.removeEventListener('scroll', this.handleScroll)
   }
 }
 </script>
